624e9b6afb687531ec4557bf41ee993f4f6e2b43
Rutuja Balasaheb Bhosale <rutujabhosale473@gmail.com>
2023-03-07 15:02:18 +0530

    dbconnect

---
+++ _dbconnect.php
@@ -0,0 +1,11 @@
+ <?php
+
+
+$servername = "sql12.freemysqlhosting.net";
+$username = "sql12599089";
+$password = "qYKI6bwqKS";
+$database = "sql12599089";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+?>
